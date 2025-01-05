5ae5a79e86d33aae9cf413732984d2da8eccbaf4
whyhussain07 <haha010403@gmail.com>
2025-01-05 15:00:19 +0500

    Update blockmesh.sh

--- blockmesh.sh
+++ blockmesh.sh
@@ -17,8 +17,7 @@ if ! command -v docker &> /dev/null; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
-fi
-
+    
 # Install Docker Compose
 echo "Installing Docker Compose..."
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
